45f2f953b6b8e3a70d7dab87eddc4c480759f26f
yfsuse@gmail.com <bmeB500!>
2014-08-01 21:24:06 +0800

    create traceconv project

--- gamelala/settings.py
+++ gamelala/settings.py
@@ -58,8 +58,12 @@ WSGI_APPLICATION = 'gamelala.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'traceconv',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
